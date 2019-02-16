7210cce411677e4f0d942ad51b2f9c209090eea2
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-02-16 14:04:34 +0100

    catch y'a b*tch

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function log(event, serveur, version) {
         .setTimestamp()
         .setFooter("JeuxGate")
         const log = client.channels.filter(c => c.name === "log" || c.name === "jg-log" || c.name === "logs" || c.name === "jg-logs" && c.guild.member(client.user).hasPermission("EMBED_LINKS"));
-        log.map(z => z.send(embed))
+        log.map(z => z.send(embed).catch(O_o=>{}))
     }else if(version === 2){
         const embed = new Discord.RichEmbed()
         .setColor(`RANDOM`)
@@ -29,7 +29,7 @@ function log(event, serveur, version) {
         .setTimestamp()
         .setFooter("JeuxGate")
         const log = client.channels.filter(c => c.name === "log" || c.name === "jg-log" || c.name === "logs" || c.name === "jg-logs" && c.guild.member(client.user).hasPermission("EMBED_LINKS"));
-        log.map(z => z.send(embed))
+        log.map(z => z.send(embed).catch(O_o=>{}))
     }
 }
 
