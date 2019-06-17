a8e735d5f97ceaa585bd6e8fb2a05b0d68588443
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-06-17 15:45:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,26 +126,6 @@ function log(event, serveur, version) {
 client.login(process.env.TOKEN)
 client.on("ready", () => {
     console.log(`connecté : ${client.user.tag}!`)
-    
-    http.createServer(function (req, res) {
-        if(req.url === "/users"){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write("" + client.users.size);
-            res.end();
-        }else if(req.url === "/vers"){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write("" + vers);
-            res.end();
-        }else if(req.url === "/guilds"){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write("" + client.guilds.size);
-            res.end();
-        }else{
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write("404");
-            res.end();
-        }
-    }).listen(80); 
 
     setInterval(function() {
 
