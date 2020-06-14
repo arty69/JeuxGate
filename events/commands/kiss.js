af74d35fe85978dbdb5e4232804c3836c635b857
Jéhèndé <jehende@jehende.fr>
2020-06-14 15:40:11 +0200

    using file for Giphy key

--- events/commands/kiss.js
+++ events/commands/kiss.js
@@ -9,12 +9,14 @@ var https = require('follow-redirects').https;
 exports.run = (message, client) =>{
     if (message.content.startsWith(prefix + "kiss")){
         log.log("[JeuxGate : Ckiss] Kiss command received from " + message.author.username + " - " + message.author.id, "kiss", message.guild.id + "/commands")
+        if(fs.readFileSync("./config/giphykey", 'utf-8') === "") return message.reply ("Je suis désolé, or la clé d'authorisation giphy est manquante, contacter le propriétaire.")
         var optionskiss = {
             'method': 'GET',
             'hostname': 'api.giphy.com',
-            'path': '/v1/gifs/search?q=kiss+anime&api_key=eYzXE0mU9zRCBLEiO0QVoom1ofMNYv2u&limit=5&offset=' + Math.floor(Math.random() * 50),
+            'path': '/v1/gifs/search?q=kiss+anime&api_key='+fs.readFileSync("./config/giphykey", 'utf-8')+'&limit=5&offset=' + Math.floor(Math.random() * 50),
             'maxRedirects': 20
         };
+        message.channel.send("<@!" + message.author.id +"> Recherche en cours de gif ...").then(y =>{
         https.request(optionskiss, function (res) {
             var chunks = [];
         
@@ -31,20 +33,23 @@ exports.run = (message, client) =>{
                 }
                 
                 
-                var kiss_embed = new Discord.RichEmbed()
+                var kiss_embed = new Discord.MessageEmbed()
                     .setColor('RANDOM')
                     .setTitle(`Tu viens d'embrasser :`)
                     .setImage(parsed["data"][Math.floor(Math.random() * parsed["pagination"].count)].images.fixed_height_small.url)
                     .setTimestamp()
                     .setFooter("JeuxGate - Powered by giphy")
-                message.channel.send(kiss_embed);
+                y.edit(kiss_embed).then(u => {
+                y.edit("")
+                })
 
         
             });
         
             res.on("error", function (error) {
-              console.error(error);
+              y.edit("Une erreure s'est produite, veuillez réessayer plus tard")
             });
         }).end()
+        })
     }
 }
