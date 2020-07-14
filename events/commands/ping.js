e9873575b80ce54403ccd67fa4387702b8fa720c
jehendeoff <jehende@jehende.fr>
2020-07-14 15:37:07 +0200

    better ping calc + ws ping

--- events/commands/ping.js
+++ events/commands/ping.js
@@ -3,11 +3,14 @@ const fs = require("fs")
 const Discord = require('discord.js');
 const prefix = fs.readFileSync("./config/prefix", "utf-8")
 
-exports.run = async (message, client) =>{
-    if(message.content.startsWith(prefix + "ping")){
-        log.log("[JeuxGate : Cping] ping command received from " + message.author.username + " - " + message.author.id, "ping", message.guild.id + "/commands")
-        
-        message.channel.send('Pong! ping :`' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-        
-    } 
+exports.run = async (message, service) =>{
+    log.log("[JeuxGate : Cping] ping command received from " + message.author.username + " - " + message.author.id, "ping", message.guild.id + "/commands")
+    if(service)
+        var x = "Le ping est probablement faussé dû au traitement du message que jeuxgate a du faire.\r\n"
+    else 
+        var x =""
+    message.channel.send(`${x}Pong! ping : \` ${Date.now() - message.createdTimestamp} ms (à peu près\` `).then (msg => {
+        msg.edit(`${x}Pong! ping : \` ${msg.createdTimestamp - message.createdTimestamp} ms\`, Latence discord : \` ${Math.round(message.client.ws.ping)} ms\` `).catch(O_o => {})
+    });
+    
 }
